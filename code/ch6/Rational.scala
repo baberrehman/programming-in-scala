aab70108237f6fb9cae271617c8f90b931102705
baberrehman <baberrehman26@gmail.com>
2022-07-12 17:39:06 +0800

    ch6

--- code/ch6/Rational.scala
+++ code/ch6/Rational.scala
@@ -1,10 +1,15 @@
 
 class Rational(n: Int, d: Int) {
-  require(d != 0)
-  val numer: Int = n
-  val denom: Int = d
 
-  override def toString = n + "/" + d
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  override def toString = numer + "/" + denom
 
   def add(that: Rational): Rational = {
     new Rational (
@@ -12,8 +17,58 @@ class Rational(n: Int, d: Int) {
         denom * that.denom
       )
   }
+
+  def lessThan(that: Rational) = {
+    this.numer * that.denom < that.numer * this.denom
+  }
+
+  def max(that: Rational) = { 
+    if (this.lessThan(that)) that else this
+  }
+
+  private def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
+  }
+
+  def + (that: Rational): Rational = {
+    new Rational(
+        numer * that.denom + that.numer * denom,
+        denom * that.denom
+      )
+  }
+
+  def + (i: Int): Rational = {
+    new Rational(numer + i * denom, denom)
+  }
+
+  def * (that: Rational): Rational = {
+    new Rational(numer * that.numer, denom * that.denom)
+  }
+
+  def * (i: Int): Rational = {
+    new Rational(numer * i, denom)
+  }
+
+  def - (that: Rational): Rational = {
+    new Rational(
+        numer * that.denom - that.numer * denom,
+        denom * that.denom
+      )
+  }
+
+  def - (i: Int): Rational = {
+    new Rational(numer - i * denom, denom)
+  }
+
+  def / (that: Rational): Rational = {
+    new Rational(numer * that.denom, denom * that.numer)
+  }
+
+  def / (i: Int): Rational = {
+    new Rational(numer, denom * i)
+  }
 }
 
 def main(args: Array[String]) : Unit = {
-
+  implicit def intToRational(x:Int): Rational = new Rational(x)
 } 
