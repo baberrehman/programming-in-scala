fad45f36767dbf00e3d4e858c885354da23025ee
baberrehman <baberrehman26@gmail.com>
2022-07-20 23:35:23 +0800

    ch12

---
+++ code/ch12/listing121.scala
@@ -0,0 +1,14 @@
+
+trait Philosophical {
+  def philosophize() = {
+    println("I consume memory, therefore I am!")
+  }
+}
+
+class Frog extends Philosophical {
+  override def toString = "green"
+}
+
+def main(args: Array[String]): Unit = {
+
+}
---
+++ code/ch12/listing124.scala
@@ -0,0 +1,18 @@
+
+trait Philosophical {
+  def philosophize() = {
+    println("I consume memory, therefore I am!")
+  }
+}
+
+class Animal
+
+trait HasLegs
+
+class Frog extends Animal with Philosophical with HasLegs {
+  override def toString = "green"
+}
+
+def main(args: Array[String]): Unit = {
+
+}
