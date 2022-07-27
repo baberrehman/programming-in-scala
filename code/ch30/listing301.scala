c524b265d5a819b25ab56577dd6e1c01eef02cbd
baberrehman <baberrehman26@gmail.com>
2022-07-27 12:11:49 +0800

    ch30

---
+++ code/ch30/listing301.scala
@@ -0,0 +1,15 @@
+
+import scala.actors._
+
+object SillyActor extends Actor {
+  def act() = {
+    for (i <- 1 to 5) {
+      println("I'm acting!")
+      Thread.sleep(1000)
+    }
+  }
+}
+
+def main(args: Array[String]): Unit = {
+
+}
